866db634a9cc2e1dcbc0763075e10dab6bcbf592
Josie <josiecollinsrc@gmail.com>
2021-03-12 14:50:34 -0600

    C:/Program Files/Git/api/notes route working

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const path = require('path');
-const jsonNotes = require('./db/db.json');
+const fs = require('fs');
+const router = require('express').Router();
+let jsonNotes = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// sets public folder to be used for js, css
-app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// sets public folder to be used for js, css
+app.use(express.static('public'));
+
 // html routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 // api routes
-// app.get('/api/notes', (req, res) => res.json(jsonNotes));
+app.get('/api/notes', (req, res) => {
+    console.log("route hit");
+    res.json(jsonNotes);
+});
+
+
+
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
